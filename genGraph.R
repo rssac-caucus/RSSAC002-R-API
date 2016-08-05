4edae329440fa3dd23b920689a67ab1355f755fe
smutt <smutt@depht.com>
2016-08-05 09:48:07 -0400

    Added GPL text at top of source files

--- genGraph.R
+++ genGraph.R
@@ -1,4 +1,21 @@
 #!/usr/bin/env Rscript --vanilla
+##  The file is part of the RSSAC002 Graphing API.
+##
+##  The RSSAC002 Graphing API is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  The RSSAC002 Graphing API is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
+##  Copyright (C) 2016, Andrew McConachie, <andrew@depht.com>
+
 rootLetters <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m")
 
 ##library(yaml, lib.loc=".") ## Tells R where to find the YAML lib
--- getYAML.sh
+++ getYAML.sh
@@ -1,4 +1,21 @@
 #!/usr/local/bin/ksh
+##  The file is part of the RSSAC002 Graphing API.
+##
+##  The RSSAC002 Graphing API is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  The RSSAC002 Graphing API is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
+##  Copyright (C) 2016, Andrew McConachie, <andrew@depht.com>
+
 # This script downloads and normalizes directory structure for RSSAC002 YAML data for all root DNS operators
 # This script is intended to be run wtihout any arguments
 
